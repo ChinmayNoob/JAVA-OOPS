ea3497261f17ebc07f6b1f069816a30ea03d23fd
ChinmayNoob <chinmaysk0408@gmail.com>
2024-03-16 12:11:16 +0530

    First hello world program

---
+++ JAVA/Main.java
@@ -0,0 +1,22 @@
+package JAVA;
+
+// Basic Structure of Java 
+
+// public class Main {
+//     public void main (){
+
+//     }
+// }
+
+// classes -> PascalNamingConvention
+// methods -> camelCasingConventions 
+
+//first program  
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+
+
